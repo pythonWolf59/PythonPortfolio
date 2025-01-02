f6aa6fe01901733a50dc280b75e7e30bc2829fd9
Hamza Sareer <humxazakir11@gmail.com>
2025-01-02 17:50:39 +0500

    Initial Commit

--- Basics.py
+++
@@ -1,6 +0,0 @@
-# Basic Python Concepts
-
-name = input("Enter your name: ")
-year_of_birth = int(input("Enter your year of birth "))
-
-print(f"{name} You would be {2024 - year_of_birth} years old in 2024")
