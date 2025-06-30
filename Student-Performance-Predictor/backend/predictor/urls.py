27c090768a58d073bde87fadf5b7b6de047c218f
pythonWolf59 <humxazakir11@gmail.com>
2025-06-30 14:47:15 +0500

    Delete Student-Performance-Predictor directory

--- Student-Performance-Predictor/backend/predictor/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import PredictGradeView
-
-urlpatterns = [
-    path('predict/', PredictGradeView.as_view(), name='predict-grade'),
-]
-# This file defines the URL patterns for the predictor app in a Django project.
